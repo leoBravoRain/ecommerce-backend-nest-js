9f7f3dd8567df6006d3b20146ba51a027f643cdb
leoBravoAgenda <98347607+leoBravoAgenda@users.noreply.github.com>
2023-03-22 12:08:36 +0000

    feat: setup initial project. Setup prisma to work with local MongoDB. Setup product module and controller. Get products endpoint

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { ProductModule } from './product/product.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ProductModule,
+    PrismaModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
